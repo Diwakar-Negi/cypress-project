313bded4fec7a16ff86a362805993fdc36359259
Diwakar-Negi <diwakar.negi@gmail.com>
2025-02-13 09:15:45 -0500

    Updated Files

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,5 +1,7 @@
-describe('Login Test', () => {
-  beforeEach(() => {
+import LoginPage from '../pageobjects/LoginPage.js';
+
+describe('Login Test using POM', () => {
+ /*  beforeEach(() => {
     cy.visit('https://test-vt-lis.azurewebsites.net/')
 });
 
@@ -9,12 +11,16 @@ describe('Login Test', () => {
       //cy.get('.btn > .k-button-text').click()
       //cy.visit('https://test-vt-lis.azurewebsites.net/registration-type')  
       })
-    })
-  })
+    }) */
 
-/*   it('should fail to log in', () => {
+it('should log in successfully', () => {
     cy.fixture('testData').then((users) => {
-      cy.login(users.invaliduser.username, users.invaliduser.password);
-      cy.get('.alert').should('be.visible');
-      })
-    }); */
+      LoginPage.visit();
+      LoginPage.enterUserame(users.invaliduser.username); 
+      LoginPage.enterPassword(users.invaliduser.password);
+      LoginPage.clcikLogin();
+      //cy.login(users.invaliduser.username, users.invaliduser.password);
+      //cy.get('.alert').should('be.visible');
+    });
+  }); 
+});
